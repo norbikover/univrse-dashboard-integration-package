02ebd30e100e5bdfab9b917f1ac4f6bb0de9ce71
Norbi Kover <norbikover@gmail.com>
2025-10-25 22:24:26 +0300

    added namespace to location-id mapping

--- Runtime/LocationIdSceneNameMapping.cs
+++ Runtime/LocationIdSceneNameMapping.cs
@@ -2,46 +2,49 @@ using UnityEngine;
 using System.Collections.Generic;
 using NaughtyAttributes;
 
-public class LocationIdSceneNameMapping : MonoBehaviour // TODO: This will be removed.
+namespace UniVRseDashboardIntegration
 {
-    #region Singleton pattern
-
-    private static LocationIdSceneNameMapping _instance;
-
-    public static LocationIdSceneNameMapping Instance
+    public class LocationIdSceneNameMapping : MonoBehaviour // TODO: This will be removed.
     {
-        get
+        #region Singleton pattern
+
+        private static LocationIdSceneNameMapping _instance;
+
+        public static LocationIdSceneNameMapping Instance
         {
-            return _instance ?? (_instance = FindAnyObjectByType<LocationIdSceneNameMapping>());
-        }
-    }
-
-    #endregion
-
-    [SerializeField] private List<Map> _mappings;
-
-    #region Getters
-    public string GetSceneNameByLocationId(string locationId)
-    {
-        foreach (Map map in _mappings)
-        {
-            if (map.LocationId.Equals(locationId))
-                return map.SceneName;
+            get
+            {
+                return _instance ?? (_instance = FindAnyObjectByType<LocationIdSceneNameMapping>());
+            }
         }
 
-        return string.Empty;
-    }
-    #endregion
-
-    [System.Serializable]
-    public class Map
-    {
-        [SerializeField] private string _locationId;
-        [SerializeField][Scene] private string _sceneName;
-
-        #region Setters and Getters
-        public string LocationId { get { return _locationId; } }
-        public string SceneName { get { return _sceneName; } }
         #endregion
+
+        [SerializeField] private List<Map> _mappings;
+
+        #region Getters
+        public string GetSceneNameByLocationId(string locationId)
+        {
+            foreach (Map map in _mappings)
+            {
+                if (map.LocationId.Equals(locationId))
+                    return map.SceneName;
+            }
+
+            return string.Empty;
+        }
+        #endregion
+
+        [System.Serializable]
+        public class Map
+        {
+            [SerializeField] private string _locationId;
+            [SerializeField][Scene] private string _sceneName;
+
+            #region Setters and Getters
+            public string LocationId { get { return _locationId; } }
+            public string SceneName { get { return _sceneName; } }
+            #endregion
+        }
     }
 }
