d67bf4755b572e2dadaa85a2ffe560f0646e7af6
Norbi Kover <norbikover@gmail.com>
2025-10-26 21:18:09 +0200

    updated networkAnalyticsEntrySender

--- Runtime/NetworkAnalyticsEntrySender.cs
+++ Runtime/NetworkAnalyticsEntrySender.cs
@@ -10,17 +10,17 @@ namespace UniVRseDashboardIntegration
         [SerializeField] private float _sendInterval = 60f;
 
         [Header("Debug")]
-        [SerializeField] private bool _debugLog = true;
+        [SerializeField] protected bool DebugLog = true;
 
         // Mandatory fields.
-        private float _totalTime;
+        protected float TotalTime;
 
         public override void OnStartClient()
         {
             base.OnStartClient();
 
             // Initialize the time since start.
-            _totalTime = 0f;
+            TotalTime = 0f;
 
             // Send an initial entry to the server and then repeatedly send updates to the server.
             if(_sendInterval > 0) InvokeRepeating(nameof(SendAnalyticsEntryToServer), 0f, _sendInterval);
@@ -30,7 +30,7 @@ namespace UniVRseDashboardIntegration
         protected virtual void Update()
         {
             // Increase the time since start.
-            _totalTime += Time.deltaTime;
+            TotalTime += Time.deltaTime;
         }
 
         [ClientCallback]
@@ -39,15 +39,15 @@ namespace UniVRseDashboardIntegration
             Dictionary<string, object> data = new Dictionary<string, object>();
 
             // Send the analytics entry to the server.
-            CmdPushAnalyticsEvent(SystemInfo.deviceUniqueIdentifier, _totalTime, JsonConvert.SerializeObject(data));
+            CmdPushAnalyticsEvent(SystemInfo.deviceUniqueIdentifier, TotalTime, JsonConvert.SerializeObject(data));
 
-            if (_debugLog) Debug.Log("Sent analytics event to server.");
+            if (DebugLog) Debug.Log("Sent analytics event to server.");
         }
 
         [Command(requiresAuthority = false)]
         private void CmdPushAnalyticsEvent(string deviceId, float totalTime, string dataJson, NetworkConnectionToClient sender = null)
         {
-            if (_debugLog) Debug.Log("[Server] Received Analytics Event data from a client. Pushing it to the cloud ....");
+            if (DebugLog) Debug.Log("[Server] Received Analytics Event data from a client. Pushing it to the cloud ....");
 
             // Send the entry's data to the cloud.
             AnalyticsEntryManager.Instance.SendAnalyticsEntryToCloud(deviceId, totalTime, JsonConvert.DeserializeObject<Dictionary<string, object>>(dataJson), (int)sender.identity.netId);
