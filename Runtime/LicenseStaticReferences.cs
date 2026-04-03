89258699a0ea429f661a5555be4901e4ad457587
Norbi Kover <norbikover@gmail.com>
2026-04-03 11:07:07 +0300

    Add comment to LicenseCode for clarity on server updates

--- Runtime/LicenseStaticReferences.cs
+++ Runtime/LicenseStaticReferences.cs
@@ -2,7 +2,7 @@ namespace UniVRseDashboardIntegration
 {
     public static class LicenseStaticReferences
     {
-        public static string LicenseCode = string.Empty;
+        public static string LicenseCode = string.Empty; // Only updated on the server.
         public static ELicenseEnvironment LicenseEnvironment = ELicenseEnvironment.DEV;
     }
 }
